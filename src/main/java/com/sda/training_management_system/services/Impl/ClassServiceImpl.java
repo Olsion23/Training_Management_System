a28ed71b7051b76317b8dc440315d316e8d7a555
Hendri2023 <durajhendri2023@gmail.com>
2023-10-22 10:56:49 +0200

    Finished class service

--- src/main/java/com/sda/training_management_system/services/Impl/ClassServiceImpl.java
+++ src/main/java/com/sda/training_management_system/services/Impl/ClassServiceImpl.java
@@ -1,7 +1,10 @@
 package com.sda.training_management_system.services.Impl;
 
 import com.sda.training_management_system.dao.Class;
+import com.sda.training_management_system.dao.Course;
+import com.sda.training_management_system.exceptions.GenericExceptions;
 import com.sda.training_management_system.repositories.ClassRepository;
+import com.sda.training_management_system.repositories.CourseRepository;
 import com.sda.training_management_system.services.ClassService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,24 +17,37 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ClassServiceImpl implements ClassService {
     private final ClassRepository classRepository;
+    private final CourseRepository courseRepository;
+
     @Override
     public Class create(Class entity) {
-        return null;
+        Class clazz = entity;
+        final Long courseId = entity.getCourse().getCourseId();
+        Course course = courseRepository.findById(courseId)
+                .orElseThrow(() -> GenericExceptions.notFound(courseId));
+        clazz.setCourse(course);
+        clazz = classRepository.save(clazz);
+        return clazz;
     }
 
     @Override
     public Class update(Class entity) {
-        return null;
+        Class clazz = this.findById(entity.getClassId());
+        if (entity.getSubject() != null)
+            clazz.setSubject(entity.getSubject());
+        clazz = classRepository.save(clazz);
+        return clazz;
     }
 
     @Override
     public Class findById(Long classId) {
-        return null;
+        return classRepository.findById(classId)
+                .orElseThrow(() -> GenericExceptions.notFound(classId));
     }
 
     @Override
     public List<Class> findAll() {
-        return null;
+        return classRepository.findAll();
     }
 
     @Override
