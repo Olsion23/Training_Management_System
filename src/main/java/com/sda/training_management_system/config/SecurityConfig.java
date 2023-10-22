284e87d7e1be1e64400a4079c0fb8feed128a0f7
Olsion23 <olsionbejleri777@gmail.com>
2023-10-22 12:11:12 +0200

    Added security config

---
+++ src/main/java/com/sda/training_management_system/config/SecurityConfig.java
@@ -0,0 +1,85 @@
+package com.sda.training_management_system.config;
+
+import com.sda.training_management_system.dao.Role;
+import com.sda.training_management_system.repositories.RoleRepository;
+import com.sda.training_management_system.security.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class SecurityConfig implements CommandLineRunner, WebMvcConfigurer {
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
+    private final RoleRepository roleRepository;
+    @Value(value = "${frontendUrl}")
+    private static String allowedUrl;
+
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService, RoleRepository roleRepository) {
+        this.userDetailsServiceImpl = userDetailsService;
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedUrl)
+                .allowedHeaders("Authorization")
+                .allowedMethods("*");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (!roleRepository.existsById("ROLE_ADMIN")){
+            Role role = new Role();
+            role.setRoleId("ROLE_ADMIN");
+            roleRepository.save(role);
+        }
+        if (!roleRepository.existsById("ROLE_LEADER")){
+            Role role = new Role();
+            role.setRoleId("ROLE_LEADER");
+            roleRepository.save(role);
+        }
+        if (!roleRepository.existsById("ROLE_PARTICIPANT")){
+            Role role = new Role();
+            role.setRoleId("ROLE_PARTICIPANT");
+            roleRepository.save(role);
+        }
+    }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder());
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+        httpSecurity.authenticationManager(authenticationManager)
+                .authorizeHttpRequests(request -> {
+                    request.anyRequest().permitAll();
+                })
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+}
