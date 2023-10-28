0c43df59fd4677ddef6949e28a8786c585f5e0da
Hendri2023 <durajhendri2023@gmail.com>
2023-10-28 15:53:24 +0200

    Added get class by user

--- src/main/java/com/sda/training_management_system/services/Impl/ClassServiceImpl.java
+++ src/main/java/com/sda/training_management_system/services/Impl/ClassServiceImpl.java
@@ -7,6 +7,7 @@ import com.sda.training_management_system.exceptions.GenericExceptions;
 import com.sda.training_management_system.repositories.ClassRepository;
 import com.sda.training_management_system.repositories.CourseRepository;
 import com.sda.training_management_system.services.ClassService;
+import com.sda.training_management_system.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +21,7 @@ import java.util.Optional;
 public class ClassServiceImpl implements ClassService {
     private final ClassRepository classRepository;
     private final CourseRepository courseRepository;
+    private UserService userService;
 
     @Override
     public Class create(Class entity) {
@@ -44,7 +46,7 @@ public class ClassServiceImpl implements ClassService {
     @Override
     public Class findById(Long classId) {
         Optional<Class> user = classRepository.findById(classId);
-        return user.orElseThrow(()-> GenericExceptions.notFound(classId));
+        return user.orElseThrow(() -> GenericExceptions.notFound(classId));
     }
 
     @Override
@@ -56,5 +58,15 @@ public class ClassServiceImpl implements ClassService {
     public String delete(Long classId) {
         classRepository.findById(classId);
         return String.format("Record with id %d deleted", classId);
+    }
+
+    @Override
+    public List<Class> getAllUserClasses() {
+        User user = userService.findUserLoggedIn();
+        if (user.getRole().getRoleId().equals("ROLE_PARTICIPANT"))
+            return user.getParticipantClasses();
+        else if (user.getRole().getRoleId().equals("ROLE_LEADER"))
+            return user.getLeaderClasses();
+        else return this.findAll();
     }
 }
